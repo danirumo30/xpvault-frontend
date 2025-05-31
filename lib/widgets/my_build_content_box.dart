c930000ca2e2f1dff422aa77374285d73393eedb
Kidou <daniel.rubio39@educa.madrid.org>
2025-05-31 19:07:55 +0200

    Scrollbars merged.

--- lib/widgets/my_build_content_box.dart
+++ lib/widgets/my_build_content_box.dart
@@ -5,7 +5,7 @@ import 'package:xpvault/themes/app_color.dart';
 import '../screens/desktop/game_detail_desktop.dart';
 import 'my_netimagecontainer.dart';
 
-class MyBuildContentBox extends StatelessWidget {
+class MyBuildContentBox extends StatefulWidget {
   final List<dynamic> items;
   final bool showBodyLabel;
 
@@ -16,8 +16,27 @@ class MyBuildContentBox extends StatelessWidget {
   });
 
   @override
+  State<MyBuildContentBox> createState() => _MyBuildContentBoxState();
+}
+
+class _MyBuildContentBoxState extends State<MyBuildContentBox> {
+  late final ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (items.isEmpty) {
+    if (widget.items.isEmpty) {
       return Container(
         height: 120,
         decoration: BoxDecoration(
@@ -47,76 +66,81 @@ class MyBuildContentBox extends StatelessWidget {
 
     return SizedBox(
       height: 180,
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        itemCount: items.length,
-        separatorBuilder: (_, __) => const SizedBox(width: 12),
-        itemBuilder: (context, index) {
-          final item = items[index];
-          final String title;
-          String? imageUrl;
-          String bodyText;
+      child: Scrollbar(
+        controller: _scrollController,
+        thumbVisibility: true,
+        trackVisibility: true,
+        child: ListView.separated(
+          controller: _scrollController,
+          scrollDirection: Axis.horizontal,
+          itemCount: widget.items.length,
+          separatorBuilder: (_, __) => const SizedBox(width: 12),
+          itemBuilder: (context, index) {
+            final item = widget.items[index];
+            final String title;
+            String? imageUrl;
+            String bodyText;
 
-          final typeStr = item.runtimeType.toString();
+            final typeStr = item.runtimeType.toString();
 
-          if (typeStr == 'Game') {
-            title = item.title;
-            imageUrl = item.screenshotUrl;
-            bodyText = "Juego";
-          } else if (typeStr == 'Movie') {
-            title = item.title;
-            imageUrl = item.posterUrl;
-            bodyText = "Película";
-          } else if (typeStr == 'Serie') {
-            title = item.title;
-            imageUrl = item.posterUrl;
-            bodyText = "Serie";
-          } else if (typeStr == 'Achievement') {
-            title = item.name ?? 'Desconocido';
-            imageUrl = item.url;
-            bodyText = "Logro";
-          } else {
-            title = 'Desconocido';
-            imageUrl = null;
-            bodyText = '';
-          }
+            if (typeStr == 'Game') {
+              title = item.title;
+              imageUrl = item.screenshotUrl;
+              bodyText = "Juego";
+            } else if (typeStr == 'Movie') {
+              title = item.title;
+              imageUrl = item.posterUrl;
+              bodyText = "Película";
+            } else if (typeStr == 'Serie') {
+              title = item.title;
+              imageUrl = item.posterUrl;
+              bodyText = "Serie";
+            } else if (typeStr == 'Achievement') {
+              title = item.name ?? 'Desconocido';
+              imageUrl = item.url;
+              bodyText = "Logro";
+            } else {
+              title = 'Desconocido';
+              imageUrl = null;
+              bodyText = '';
+            }
 
-          void Function()? onTapHandler;
-          if (typeStr == 'Game') {
-            onTapHandler = () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      GameDetailDesktopPage(steamId: item.steamId),
-                ),
-              );
-            };
-          } else if (typeStr == 'Movie') {
-            onTapHandler = () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      MovieDetailDesktopPage(movieId: item.tmbdId),
-                ),
-              );
-            };
-          } else {
-            onTapHandler = null;
-          }
+            void Function()? onTapHandler;
+            if (typeStr == 'Game') {
+              onTapHandler = () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        GameDetailDesktopPage(steamId: item.steamId),
+                  ),
+                );
+              };
+            } else if (typeStr == 'Movie') {
+              onTapHandler = () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        MovieDetailDesktopPage(movieId: item.tmbdId),
+                  ),
+                );
+              };
+            } else {
+              onTapHandler = null;
+            }
 
-
-          return SizedBox(
-            width: 120,
-            child: MyNetImageContainer(
-              title: title,
-              body: showBodyLabel ? bodyText : '',
-              image: imageUrl ?? '',
-              onTap: onTapHandler,
-            ),
-          );
-        },
+            return SizedBox(
+              width: 120,
+              child: MyNetImageContainer(
+                title: title,
+                body: widget.showBodyLabel ? bodyText : '',
+                image: imageUrl ?? '',
+                onTap: onTapHandler,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
