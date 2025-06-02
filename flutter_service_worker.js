90c4a00833375f149fc0bdd8a6fc696408eeee63
danirumo30 <danirumo30@users.noreply.github.com>
2025-06-02 12:20:13 +0000

    deploy: e3519e3f9a12fe6379d3178ae04db1b569254350

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,8 +39,8 @@ const RESOURCES = {"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "index.html": "95d86986471adc67f1cb9db033606cd1",
 "/": "95d86986471adc67f1cb9db033606cd1",
 "version.json": "3e55bbaa1593815144277a24352e7966",
-"flutter_bootstrap.js": "ee156190ef278686e16323e372dee8c7",
-"main.dart.js": "f70de85cbb0fa7a82364c960cebff2d4",
+"flutter_bootstrap.js": "de228f281e90e53f551d23b0435aebe8",
+"main.dart.js": "3ee2829477ad2bacb17dee89c14bb856",
 "favicon.png": "5381ce510bdcc7e63e48a573228ee179"};
 // The application shell files that are downloaded before a service worker can
 // start.
