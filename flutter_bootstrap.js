0bbde782f30a5ff422a69a28724e0f77b72198a7
danirumo30 <danirumo30@users.noreply.github.com>
2025-06-07 13:42:50 +0000

    deploy: f6c110119f97f973e3e95b583c2e2054513ab322

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2979455038"
+    serviceWorkerVersion: "2984293622"
   }
 });
